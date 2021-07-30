c092eb7747801a48c494d5d74814989c482f313a
VaibhavBagaria <76587881+VaibhavBagaria@users.noreply.github.com>
2021-07-30 13:12:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+MousX = 0;
+MousY = 0;
+MImage = ""
+function preload() {
+    MImage = loadImage("Moustache.jpg")
+}
+
+function setup() {
+    canvas = createCanvas(370, 370)
+    canvas.center()
+    Video = createCapture(VIDEO)
+    Video.hide()
+    PoseNet = ml5.poseNet(Video, Model_Loaded)
+    PoseNet.on('pose', GotPoses)
+}
+
+function draw() {
+    image(Video, 0, 0, 370, 370)
+    image(MImage, MousX, MousY, 40, 40)
+}
+
+function Take_Snapshot() {
+    save("Moustashe.png")
+}
+
+
+function Model_Loaded() {
+    console.log("model is loaded");
+}
+
+function GotPoses(results) {
+    if (results.length > 0) {
+        console.log(results)
+        MousX = results[0].pose.nose.x - 180
+        MousY = results[0].pose.nose.y - 68
+    }
+}
